c78a4e169d2ae7ac330c685e06735c05fbcecd97
GuiMonLopes <guilherme.monteiro1@gmail.com>
2024-11-17 15:19:40 -0300

    ultimas mudanças

--- setup/settings.py
+++ setup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','localhost']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
